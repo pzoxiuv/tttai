175d13e0ad584b890b40b25874036a559a21e9b7
Alex Merenstein <alex.merenstein@gmail.com>
2014-03-22 11:13:44 -0400

    Removes old sort function.

--- luaai.lua
+++ luaai.lua
@@ -1,37 +1,6 @@
 require "my_opencv"
 
-function spairs(t, order)
-    local keys = {}
-    for k in pairs(t) do keys[#keys+1] = k end
-
-    if order then
-        table.sort(keys, function(a, b) return order(t, a, b) end)
-    else
-        table.sort(keys)
-    end
-
-    local i=0
-    return function()
-        i = i + 1
-        if keys[i] then
-            return keys[i], t[keys[i]]
-        end
-    end
-end
-
 coords = detectAndDisplay("full_shot5.png", "single_square.xml")
-
-for i, t in spairs(coords, function(t, a, b)
-        if (t[b][2] > t[a][2]) then
-            return true
-        elseif (t[b][2] < t[a][2]) then
-            return false
-        else
-            return t[b][1] > t[a][1]
-        end
-    end) do
---    print ("x: " .. t[1] .. " y: " .. t[2])
-end
 
 table.sort(coords,
 	function(a, b)
